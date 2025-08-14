3a23bd7dad38303c72e7615a1755cfbcf21755be
Sushil Kandel <sushil.kandel16@gmail.com>
2025-08-15 09:44:23 +1000

    Setting up app.js

---
+++ backend/ecommerce_backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) =>{
+    res.send("Welcome");
+});
+
+app.listen(port, ()=>{
+    console.log("Server is starting on port ", port);
+});
