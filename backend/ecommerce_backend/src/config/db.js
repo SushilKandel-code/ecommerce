86b772b8a29734f0f5afa585241848d77a57e13f
Sushil Kandel <sushil.kandel16@gmail.com>
2025-08-15 14:34:46 +1000

    little change

--- backend/ecommerce_backend/src/config/db.js
+++ backend/ecommerce_backend/src/config/db.js
@@ -4,21 +4,21 @@ env.config();
 
 
 const db = new pg.Client({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:String(process.env.DB_PASSWORD),
-    database:process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: String(process.env.DB_PASSWORD),
+    database: process.env.DB_DATABASE,
 });
 
-const connectDB = async()=>{
-    try{
+const connectDB = async () => {
+    try {
         await db.connect();
         console.log("Database connected Successfully..");
-    }catch(error){
-    console.error("Something went wrong", error);
-    process.exit(1);
+    } catch (error) {
+        console.error("Something went wrong", error);
+        process.exit(1);
     }
 }
 
-export {connectDB}
+export { connectDB }
